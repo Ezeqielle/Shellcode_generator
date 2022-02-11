314d5365b14d4f5ce55e8a63138b4f457666c149
Ezeqielle <mathis.dimascio@gmail.com>
2022-02-11 17:24:47 +0100

    update:: add script/compile_payload.py

---
+++ script/compile_payload.py
@@ -0,0 +1,16 @@
+import subprocess
+
+# payload = "chemain vers le fichier payload.asm"
+
+def compile(payload):
+    cmd = "nasm -f elf64 -o out.o "+ payload +".asm; ld -o payload out.o"
+    subprocess.call(cmd, shell=True)
+
+def get_payload_hex():
+    cmd = "for i in $(objdump -d payload |grep '^ ' |cut -f2); do echo -n '\x'$i; done; echo"
+    payloadHex = subprocess.call(cmd, shell=True)
+    return payloadHex
+
+def move_payload():
+    cmd = "mv payload ../output/"
+    subprocess.call(cmd, shell=True)
