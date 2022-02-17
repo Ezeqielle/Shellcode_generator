2ef85f46dc9cb8bf71f12afc203d9fcc493fd923
Ezeqielle <mathis.dimascio@gmail.com>
2022-02-17 18:54:23 +0100

    update:: add commentary

--- script/compile_payload.py
+++ script/compile_payload.py
@@ -2,15 +2,18 @@ import subprocess
 
 # payload = "chemain vers le fichier payload.asm"
 
+# Compile the payload
 def compile_pay(payload):
     cmd = "nasm -f elf64 -o out.o "+ payload +".asm; ld -o payload out.o"
     subprocess.call(cmd, shell=True)
 
+# Get the payload hex
 def get_payload_hex():
     cmd = "for i in $(objdump -d payload |grep '^ ' |cut -f2); do echo -n '\x'$i; done; echo"
     payloadHex = subprocess.call(cmd, shell=True)
     return payloadHex
 
+# Move the payload
 def move_payload():
     cmd = "mv payload ../output/"
     subprocess.call(cmd, shell=True)
